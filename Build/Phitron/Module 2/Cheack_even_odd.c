d469c12e06a6d739accc832fafb76098d9a1ff4e
Md. Abrar Siddiqui <123486091+Abraralvi2002@users.noreply.github.com>
2025-04-06 23:14:12 +0600

    Add files via upload

---
+++ Build/Phitron/Module 2/Cheack_even_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("The number is Even.");
+    }else{
+        printf("The number is Odd.");
+    }
+
+    return 0;
+
+
+}
---
+++ Build/Phitron/Module 2/Cheacking_positive_negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int s;
+    scanf("%d",&s);
+    if(s>=0){
+        printf("The number is positive integer.");
+
+    }else{
+        printf("The number is negative integer.");
+    }
+    return 0;
+}
