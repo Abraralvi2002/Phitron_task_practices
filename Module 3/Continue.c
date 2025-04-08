be6cc388198a98a18847c2b75cdb63cd1a6f1841
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-04-08 22:42:49 +0600

    even odd in loop using continue

---
+++ Module 3/Continue.c
@@ -0,0 +1,11 @@
+//even odd with continue....
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++){
+        if(i%2==0){
+            continue;
+        }else{
+            printf("%d\n",i);
+        }
+    }
+}
