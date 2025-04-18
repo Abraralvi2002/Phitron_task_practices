5ed9db0fb8a70bad37323be94a5830464055e99b
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-04-18 18:17:33 +0600

    problem 5 solved

--- Module 6/D_Fixed_Password.c
+++ Module 6/D_Fixed_Password.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
-    
+    int n;
+    for(int i = 1;;i++){
+        scanf("%d",&n);
+        if(n==1999){
+            printf("Correct");
+            break;
+        }else{
+            printf("Wrong\n");
+        }
+    }
     return 0;
 }
