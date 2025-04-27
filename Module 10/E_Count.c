40faa34ddd47f470fb269871dee22f67cda5de51
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-04-27 23:14:25 +0600

    problem solved

---
+++ Module 10/E_Count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[1000001];
+    gets(s);
+    int length = strlen(s);
+    int sum = 0;
+    for(int i =0;i<length;i++){
+        sum += s[i] - 48;
+    }
+    printf("%d",sum);
+    return 0;
+}
