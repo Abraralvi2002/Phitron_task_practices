dd342b7f0a6ef299b8fea7e5c7c39478a268134f
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-06 22:05:11 +0600

    Downside  pyramid pattern

--- Practice Module 5/Right_down_stars.c
+++ Practice Module 5/Right_down_stars.c
@@ -2,17 +2,18 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int star = n;
+    int star = n+2;
     int space = 1;
     for(int i = 1;i<=n;i++){
         for(int j = 1;j<=space;j++){
             printf(" ");
         }
-        for(int j = 1;j<=star-1;j++){
+
+        for(int j = 1;j<=star;j++){
             printf("*");
         }
         printf("\n");
-        star--;
+        star -= 2;
         space++;
     }
     return 0;
